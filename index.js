20054c2a859a4da02ef86eb4175df973d62c3f47
Paridhi Shah <paridhi.shah@cs.christuniversity.in>
2021-08-24 15:31:12 +0530

    add playwright builpack

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ cron.schedule('* * * * *', () => {
 
 
 app.listen(app.get('port'), () => {
-  console.log(`slot machine tests. Please wait script has started!`)
+  console.log(`slot machine tests.Please wait script has started!`)
 })
